4b24d77c295b1eb0e7217d463c2b97ce00732703
Travis Mix <travismix1980@gmail.com>
2024-06-10 14:20:52 -0700

    Completed W01 Code: Dynamic Arrays

--- week01/analyze/Sorting.cs
+++ week01/analyze/Sorting.cs
@@ -1,17 +1,23 @@
-﻿public static class Sorting {
-    public static void Run() {
+﻿public static class Sorting
+{
+    public static void Run()
+    {
         var numbers = new[] { 3, 2, 1, 6, 4, 9, 8 };
         SortArray(numbers);
         Console.Out.WriteLine("int[]{{{0}}}", string.Join(", ", numbers)); //int[]{1, 2, 3, 4, 6, 8, 9}
     }
 
-    private static void SortArray(int[] data) {
-        for (var sortPos = data.Length - 1; sortPos >= 0; sortPos--) {
-            for (var swapPos = 0; swapPos < sortPos; ++swapPos) {
-                if (data[swapPos] > data[swapPos + 1]) {
+    private static void SortArray(int[] data)
+    {
+        for (var sortPos = data.Length - 1; sortPos >= 0; sortPos--)
+        {
+            for (var swapPos = 0; swapPos < sortPos; ++swapPos)
+            {
+                if (data[swapPos] > data[swapPos + 1])
+                {
                     (data[swapPos + 1], data[swapPos]) = (data[swapPos], data[swapPos + 1]);
                 }
             }
         }
     }
-}
+} // O(n )
--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -1,8 +1,12 @@
-public static class ArraysTester {
+using System.Security.Cryptography.X509Certificates;
+
+public static class ArraysTester
+{
     /// <summary>
     /// Entry point for the tests
     /// </summary>
-    public static void Run() {
+    public static void Run()
+    {
         // Sample Test Cases (may not be comprehensive)
         Console.WriteLine("\n=========== PROBLEM 1 TESTS ===========");
         double[] multiples = MultiplesOf(7, 5);
@@ -27,35 +31,59 @@ public static class ArraysTester {
         Console.WriteLine($"<List>{{{string.Join(',', numbers)}}}"); // <List>{1, 2, 3, 4, 5, 6, 7, 8, 9}
     }
     /// <summary>
-    /// This function will produce a list of size 'length' starting with 'number' followed by multiples of 'number'.  For 
+    /// This function will produce a list of size 'length' starting with 'number' followed by multiples of 'number'.  For
     /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
-    private static double[] MultiplesOf(double number, int length)
+    ///
+    /// To implement MultiplesOf is a pretty simple solution
+    /// step 1 create a new array called results
+    /// step 2 write a for loop that runs 1 < length
+    /// step 3 in the for loop add the result of number times i to our list
+    /// step 4 return the array we created
+    private static double[] MultiplesOf(double number, int length) // cannot return a list from the method that is expecting an array and yet the assignment specified a list here so changed the return type of the method
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        /// To implement MultiplesOf is a pretty simple solution
+        /// step 1 create a new array called results
+        double[] results = new double[length];
+        /// step 2 write a for loop that runs 1 < length
+        for (var i = 1; i <= length; i++)
+        {
+            /// step 3 in the for loop add the result of number times i to our array
+            results[i - 1] = number * i;
+        }
 
-        return new double[0]; // replace this return statement with your own
+        /// step 4 return the array we created
+        return results;
     }
-    
+
     /// <summary>
-    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
-    /// <c>&lt;List&gt;{1, 2, 3, 4, 5, 6, 7, 8, 9}</c> and an amount is 3 then the list returned should be 
-    /// <c>&lt;List&gt;{7, 8, 9, 1, 2, 3, 4, 5, 6}</c>.  The value of amount will be in the range of <c>1</c> and 
+    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is
+    /// <c>&lt;List&gt;{1, 2, 3, 4, 5, 6, 7, 8, 9}</c> and an amount is 3 then the list returned should be
+    /// <c>&lt;List&gt;{7, 8, 9, 1, 2, 3, 4, 5, 6}</c>.  The value of amount will be in the range of <c>1</c> and
     /// <c>data.Count</c>.
     /// <br /><br />
     /// Because a list is dynamic, this function will modify the existing <c>data</c> list rather than returning a new list.
     /// </summary>
+    ///
+    /// Another simple problem to solve.
+    /// step 1 create a temp list to store our data in that needs rotated called temp
+    /// step 2 create an int variable called amountToMove to store data.Count - amount which tells us what actually      needs moved
+    /// step 3 using GetRange(0, data.Count - amount) store all the need to be moved items in to the temp list
+    /// step 4 using RemoveRange(0, data.Count - amount) remove the data that needs rotated from data
+    /// step 5 using AddRange(temp)
     private static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
-
+        /// step 1 create a temp list to store our data in that needs rotated called temp
+        List<int> temp = new();
+        /// step 2 create an int variable called amountToMove to store data.Count - amount which tells us what actually needs moved
+        int amountToMove = data.Count - amount;
+        /// step 3 using GetRange(0, data.Count - amount) store all the need to be moved items in to the temp list
+        temp.AddRange(data.GetRange(0, amountToMove));
+        /// step 4 using RemoveRange(0, data.Count - amount) remove the data that needs rotated from data
+        data.RemoveRange(0, amountToMove);
+        /// step 5 using AddRange(temp)
+        data.AddRange(temp);
     }
 }
