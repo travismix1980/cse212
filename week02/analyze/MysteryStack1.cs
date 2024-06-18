ac7c56ab3a791f7e888b52000bf3756f9edfa672
Travis Mix <travismix1980@gmail.com>
2024-06-18 02:36:27 -0700

    Week 02 Analyze Stacks Complete

--- week02/analyze/MysteryStack1.cs
+++ week02/analyze/MysteryStack1.cs
@@ -1,4 +1,10 @@
-﻿public static class MysteryStack1 {
+﻿/*
+so to me it would seem that this code will take in some text as a string
+and then spit it back out in reverse.  This could be useful for reversing
+strings.
+*/
+
+public static class MysteryStack1 {
     public static string Run(string text) {
         var stack = new Stack<char>();
         foreach (var letter in text)
--- week02/analyze/MysteryStack2.cs
+++ week02/analyze/MysteryStack2.cs
@@ -1,4 +1,24 @@
-﻿public static class MysteryStack2 {
+﻿/*
+This seems to be a class that uses a stack to perform as a calculator.
+It clearly checks for numbers and also special mathmatical symbols to determine
+if we need to add subtract multiply or divide those numbers
+
+For problem two we get Determine the result of the function if the following inputs are provided
+
+1) 5 3 7 + *  I would say that the result will be 50 based on the input set of characters.  The stack
+should pop off ast * + 7 3 5 if we are just popping things off the end so lets go through the code step by step
+first we would get the + symbol and then pop off and store 7 and 3 which is 10.  Then we store 10 in res and put
+that back on the stack.  Next we go through the stack again and we get the * symbol for multiplication and we have the numbers 5 and 10 left so 5 * 10 is 50.
+
+2) 6 2 + 5 3 - / and I would say the result would be 4.  So first we loop through and find the + and we have 6 and 2 making 6 + 2 = 8 and store 8 back on the stack.  this gives us 8 5 3 - / so we then subtrack 3 from 5 = 2 which goes back in the stack for a 8 -2 / so we then divide 8 by 2 = 4
+
+3) Invalid Case 1 is caused by a stack count of less than 2 so input characters could be just a -
+   Invalid Case 2 is a divide by zero error so if our inputs were 0 3 /
+   Invalid Case 3 seems to be cause by trying to do math with an empty string so 2 4 "invalid character to search for so not a + - / *" maybe 2 4 ^
+   Invalid Case 4 is caused by stack.Count not equaling 1 at the end of a loop through cycle. so maybe an input of 1 2 + -
+*/
+
+public static class MysteryStack2 {
     private static bool IsFloat(string text) {
         return float.TryParse(text, out _);
     }
