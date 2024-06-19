0e66c197e918c4d71e0e78809b5d3273dc76aa71
Travis Mix <travismix1980@gmail.com>
2024-06-19 13:25:21 -0700

    Done with Week 02 Team Assignment

--- week02/teach/CustomerService.cs
+++ week02/teach/CustomerService.cs
@@ -1,5 +1,5 @@
 ﻿/// <summary>
-/// Maintain a Customer Service Queue.  Allows new customers to be 
+/// Maintain a Customer Service Queue.  Allows new customers to be
 /// added and allows customers to be serviced.
 /// </summary>
 public class CustomerService {
@@ -11,24 +11,46 @@ public class CustomerService {
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: make a queue and make sure you are able to get an error message if you add more people than queue than the queue can handle.  Also check to make sure we can add new customers
+        // Expected Result: should  get an error message about going over max queue size and we should be able to add customers
         Console.WriteLine("Test 1");
 
-        // Defect(s) Found: 
+        var q1 = new CustomerService(2);
+        q1.AddNewCustomer();
+        q1.AddNewCustomer();
+        q1.AddNewCustomer();
+
+        // Defect(s) Found: no error message when going over max queue size error check happening before attempting to add to queue and checking for a number that was higher than maxsize.  changed check to make sure we are checking for maxsize
 
         Console.WriteLine("=================");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: can we actually serve customers and when no more customers in queue do we get a empty queue message
+        // Expected Result: with 2 customers looping through 3 times we should get 2 customers of output and then an empty queue message so customer1 data, customer2 data,  empty queue message
         Console.WriteLine("Test 2");
-
-        // Defect(s) Found: 
+        var q2 = new CustomerService(2);
+        q2.AddNewCustomer();
+        q2.AddNewCustomer();
+        for(int i = 0; i < 3; i++){
+            q2.ServeCustomer();
+        }
+        // Defect(s) Found: no error message and we are removing customers before outputting their data
 
         Console.WriteLine("=================");
 
         // Add more Test Cases As Needed Below
+        // test 3
+        // Scenario: if maxqueue size is set to 0 or is invalid is does it should be set to 10 by default
+        // Expected Result: maxqueue size should be set to 10
+
+        Console.WriteLine("Test 3");
+        var q3 = new CustomerService(0);
+        Console.WriteLine($"Max Size should equal 10 and is actually {q3._maxSize}");
+
+        // Defect(s) Found: none
+
+        Console.WriteLine("=================");
+
     }
 
     private readonly List<Customer> _queue = new();
@@ -62,12 +84,12 @@ public class CustomerService {
     }
 
     /// <summary>
-    /// Prompt the user for the customer and problem information.  Put the 
+    /// Prompt the user for the customer and problem information.  Put the
     /// new record into the queue.
     /// </summary>
     private void AddNewCustomer() {
         // Verify there is room in the service queue
-        if (_queue.Count > _maxSize) {
+        if (_queue.Count >= _maxSize) {
             Console.WriteLine("Maximum Number of Customers in Queue.");
             return;
         }
@@ -88,14 +110,21 @@ public class CustomerService {
     /// Dequeue the next customer and display the information.
     /// </summary>
     private void ServeCustomer() {
-        _queue.RemoveAt(0);
-        var customer = _queue[0];
-        Console.WriteLine(customer);
+        if(_queue.Count > 0)
+        {
+            var customer = _queue[0];
+            _queue.RemoveAt(0);
+            Console.WriteLine(customer);
+        }
+        else
+        {
+            Console.WriteLine("Queue is Empty");
+        }
     }
 
     /// <summary>
     /// Support the WriteLine function to provide a string representation of the
-    /// customer service queue object. This is useful for debugging. If you have a 
+    /// customer service queue object. This is useful for debugging. If you have a
     /// CustomerService object called cs, then you run Console.WriteLine(cs) to
     /// see the contents.
     /// </summary>
