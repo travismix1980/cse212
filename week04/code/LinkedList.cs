7b22d50edce09bc7bc9e7cf9d8ed4b61d18d8a89
Travis Mix <travismix1980@gmail.com>
2024-06-30 21:20:44 -0700

    Finished Week 4 Code

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Transactions;
 
 public class LinkedList : IEnumerable<int> {
     private Node? _head;
@@ -28,6 +29,18 @@ public class LinkedList : IEnumerable<int> {
     /// </summary>
     public void InsertTail(int value) {
         // TODO Problem 1
+        Node newNode = new(value);
+        if(_head is null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            newNode.Prev = _tail;
+            _tail!.Next = newNode;
+            _tail = newNode;
+        }
     }
 
 
@@ -35,7 +48,7 @@ public class LinkedList : IEnumerable<int> {
     /// Remove the first node (i.e. the head) of the linked list.
     /// </summary>
     public void RemoveHead() {
-        // If the list has only one item in it, then set head and tail 
+        // If the list has only one item in it, then set head and tail
         // to null resulting in an empty list.  This condition will also
         // cover an empty list.  Its okay to set to null again.
         if (_head == _tail) {
@@ -56,13 +69,23 @@ public class LinkedList : IEnumerable<int> {
     /// </summary>
     public void RemoveTail() {
         // TODO Problem 2
+        if(_head == _tail)
+        {
+            _head = null;
+            _tail = null;
+        }
+        else if(_head is not null)
+        {
+            _tail!.Prev!.Next = null;
+            _tail = _tail.Prev;
+        }
     }
 
     /// <summary>
     /// Insert 'newValue' after the first occurrence of 'value' in the linked list.
     /// </summary>
     public void InsertAfter(int value, int newValue) {
-        // Search for the node that matches 'value' by starting at the 
+        // Search for the node that matches 'value' by starting at the
         // head of the list.
         Node? curr = _head;
         while (curr is not null) {
@@ -72,7 +95,7 @@ public class LinkedList : IEnumerable<int> {
                 if (curr == _tail) {
                     InsertTail(newValue);
                 }
-                // For any other location of 'value', need to create a 
+                // For any other location of 'value', need to create a
                 // new node and reconnect the links to insert.
                 else {
                     Node newNode = new(newValue);
@@ -94,6 +117,28 @@ public class LinkedList : IEnumerable<int> {
     /// </summary>
     public void Remove(int value) {
         // TODO Problem 3
+        Node? curr = _head;
+        while(curr is not null)
+        {
+            if(curr.Data == value)
+            {
+                if(curr == _tail)
+                {
+                    RemoveTail();
+                }
+                else if(curr == _head)
+                {
+                    RemoveHead();
+                }
+                else
+                {
+                    curr.Next!.Prev = curr.Prev;
+                    curr.Prev!.Next = curr.Next;
+                }
+                return;
+            }
+            curr = curr.Next;
+        }
     }
 
     /// <summary>
@@ -101,6 +146,15 @@ public class LinkedList : IEnumerable<int> {
     /// </summary>
     public void Replace(int oldValue, int newValue) {
         // TODO Problem 4
+        Node? curr = _head;
+        while(curr is not null)
+        {
+            if(curr.Data == oldValue)
+            {
+                curr.Data = newValue;
+            }
+            curr = curr.Next;
+        }
     }
 
     /// <summary>
@@ -127,7 +181,12 @@ public class LinkedList : IEnumerable<int> {
     /// </summary>
     public IEnumerable Reverse() {
         // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
+        Node? curr = _tail;
+        while(curr is not null)
+        {
+            yield return curr.Data; // replace this line with the correct yield return statement(s)
+            curr = curr.Prev;
+        }
     }
 
     public override string ToString() {
